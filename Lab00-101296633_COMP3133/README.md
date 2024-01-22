465d287266a8ea944eb952697c632c8e58621c32
Ryan Lee <ryan.khlee@yahoo.ca>
2024-01-22 17:57:14 -0500

    Addition of the Lab Week 1 Intro/ Review of Full Stack Development exercises

---
+++ Lab00-101296633_COMP3133/README.md
@@ -0,0 +1,14 @@
+# Introduction to Full Stack Development
+
+## Description
+Quick refresher exercises on basic Node.js methods, functions, and commands. Introduction on the topic of Buffers and their importance as well as ES6 JavaScript syntax & code.
+
+## Review
+Here are the bash/ terminal commands to create a Node.js Project directory
+```mkdir <directory_name>```
+Next you should enter into the directory you wish to host your node project using this command
+```cd <directory_name>```
+This line of code initializes the directory as a node project
+```npm init -y```
+Now ths command will open an editor for you to work on your project
+```code .```
---
+++ Lab00-101296633_COMP3133/index.js
@@ -0,0 +1,42 @@
+/*
+ __          __       _     ___  __ 
+ \ \        / /      | |   / _ \/_ |
+  \ \  /\  / /__  ___| | _| | | || |
+   \ \/  \/ / _ \/ _ \ |/ / | | || |
+    \  /\  /  __/  __/   <| |_| || |
+     \/  \/ \___|\___|_|\_\\___/ |_|
+*/
+
+console.log('Week01 - Node.js Buffers, interation, TypedArrays, and character encodings');
+
+//Useful to confirm directory structure, file names & path
+console.log(__dirname);
+console.log(__filename);
+console.log(process.cwd());
+
+//Displays all enviromental variables in our directory
+//console.log(process.env);
+//console.log(process)
+
+//Creating the buffer 10 bytes 
+var buf = new Buffer(10)          //Technique 1
+// var buf = Buffer.alloc(10)     //Technique 2
+
+console.log(buf)
+buf = Buffer.from("Hello World")
+console.log(buf)
+console.log(buf.toString())
+console.log(buf[0])
+console.log(buf[1])
+console.log(buf[7])
+buf[7] = 0x66
+console.log(buf.length)
+
+const str = '💀😈';
+var buf1 = Buffer.from(str)
+console.log(buf1.length)
+console.log(buf1[0])
+console.log(buf1[1])
+console.log(buf1.toString())
+console.log(buf1.toString('hex'))
+console.log(buf1.toJSON())
