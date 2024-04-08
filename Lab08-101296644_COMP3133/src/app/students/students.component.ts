04fcddce1b06ef0544a68c4061bcff8fab337c54
Ryan Lee <ryan.khlee@yahoo.ca>
2024-04-07 22:46:23 -0400

    Lab 8 components and services

---
+++ Lab08-101296644_COMP3133/src/app/students/students.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { DateService } from '../date.service';
+import { DatePipe } from '@angular/common';
+
+@Component({
+  selector: 'app-students',
+  standalone: true,
+  imports: [],
+  templateUrl: './students.component.html',
+  styleUrl: './students.component.css'
+})
+export class StudentsComponent {
+  
+}
